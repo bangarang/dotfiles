7505f28b7ed2fab3e5fc6ce25d828c11df08a569
Alex Rock <hi@lxrck.com>
2016-11-30 00:24:40 -0600

    ../..

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,3 +14,11 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+
+
+function clone() {
+    git clone --depth=1 $1
+    cd $(basename ${1%.*})
+    yarn install
+}
+alias push="git push"
